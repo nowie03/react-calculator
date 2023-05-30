5cbaeb30640acc7932264cff0bb61398a3cf946e
Immanuel <nowpertimmanuel@gmail.com>
2023-05-30 10:14:17 +0530

    completed calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,94 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //single state variable to hold state of the application
+  const [input,setInput]=useState({
+    operandLeft:0,
+    operandRight:0,
+    result:0,
+    operator:'+'
+  })
+
+//operator handler
+  const operatorHandler =(event)=>{
+    switch(event.target.value){
+      case "+":
+        setInput({...input,operator:"+"});
+        break;
+      case "-":
+        setInput({...input,operator:"-"});
+        break;
+      case "*":
+        setInput({...input,operator:"*"});
+        break;
+      case "/":
+        setInput({...input,operator:"/"});
+        break;
+      default:
+        break;
+    }
+    
+  }
+
+//calculate handler
+  const calculateHandler=({operandLeft,operandRight,operator}) => {
+    console.log(operandLeft+" "+operandRight);
+    let result;
+    switch(operator){
+      case "+":
+        result=operandLeft+operandRight;
+        break;
+      case "-":
+        result=operandLeft-operandRight;
+        break;
+      case "*":
+        result=operandLeft*operandRight;
+        break;
+      case "/":
+        result=operandLeft/operandRight;
+        break;
+      default:
+        break;
+    }
+    
+    setInput({
+      ...input,
+        result:result
+      })
+    
+  }
+
+  
+
+//use effect to automatically calculate result on input change
+  useEffect(()=>{
+    calculateHandler(input);
+  },[input.operandLeft, input.operandRight,input.operator])  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="calculator-wrap">
+       <div className="operand-section">
+       <input type="number" id="operand-left" placeholder={input.operandLeft} onClick={(e)=>setInput({...input,
+      operandLeft:parseInt(e.target.value)})}/>
+       <span className="operator">{input.operator}</span>
+       <input type="number" id="operand-right" placeholder ={input.operandRight} onClick={(e)=>setInput({...input,
+      operandRight:parseInt(e.target.value)})}/>
+       <span id="equal sign">=</span>
+       <input type="number" id="operand-right" readOnly value={input.result}/>
+
+       </div>
+       <div className="operator-section">
+        <button className="operator" onClick={(e)=>operatorHandler(e)}value="+">+</button>
+        <button className="operator" onClick={(e)=>operatorHandler(e)}value="-">-</button>
+        <button className="operator" onClick={(e)=>operatorHandler(e)}value="*"> *</button>
+        <button className="operator" onClick={(e)=>operatorHandler(e)}value="/">/</button>
+       </div>
+       <button className="submit" onClick={(e)=>calculateHandler(input)}>calculate</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
